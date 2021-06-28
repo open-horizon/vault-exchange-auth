7342bca28a6a988798518a9082a13c3bc836f917
Dave Booz <booz@us.ibm.com>
2021-06-28 08:49:36 -0400

    Issue 9 - Support kv v2 backend
    Signed-off-by: Dave Booz <booz@us.ibm.com>

--- plugin/policy.go
+++ plugin/policy.go
@@ -13,15 +13,18 @@ const USER_POLICY_NAME = `openhorizon-%s-%s`
 const AGBOT_POLICY = `path "openhorizon/*" {capabilities = ["list","read"]}`
 
 // Openhorizon org admins will have these ACL policies attached.
-const ADMIN_ORG_WIDE_POLICY = `path "openhorizon/%s/*" {capabilities = ["list", "read", "create", "delete", "update"]}`
-const ADMIN_USER_PRIVATE_POLICY = `path "openhorizon/%s/user/*" {capabilities = ["delete"]}`
+const ADMIN_ORG_WIDE_POLICY1 = `path "openhorizon/data/%s/*" {capabilities = ["create", "update"]}`
+const ADMIN_ORG_WIDE_POLICY2 = `path "openhorizon/metadata/%s/*" {capabilities = ["list", "read", "delete"]}`
+const ADMIN_USER_PRIVATE_POLICY = `path "openhorizon/metadata/%s/user/*" {capabilities = ["delete"]}`
 
 // Regular openhorizon users will have these ACL policies attached.
-const NON_ADMIN_ORG_WIDE_POLICY = `path "openhorizon/%s/*" {capabilities = ["list"]}`
-const USER_PRIVATE_DENY_POLICY = `path "openhorizon/%s/user/*" {capabilities = ["deny"]}`
+const NON_ADMIN_ORG_WIDE_POLICY = `path "openhorizon/metadata/%s/*" {capabilities = ["list"]}`
+const USER_PRIVATE_DENY_POLICY1 = `path "openhorizon/data/%s/user/*" {capabilities = ["deny"]}`
+const USER_PRIVATE_DENY_POLICY2 = `path "openhorizon/metadata/%s/user/*" {capabilities = ["deny"]}`
 
 // All supported openhorizon users will have these policies attached.
-const USER_PRIVATE_POLICY = `path "openhorizon/%s/user/%s/*" {capabilities = ["list", "read", "create", "delete", "update"]}`
+const USER_PRIVATE_POLICY1 = `path "openhorizon/data/%s/user/%s/*" {capabilities = ["create", "update"]}`
+const USER_PRIVATE_POLICY2 = `path "openhorizon/metadata/%s/user/%s/*" {capabilities = ["list", "read", "delete"]}`
 
 // Ensure that the right ACL policies exist so that they can be attached to the user's token.
 func (o *ohAuthPlugin) setupUserPolicies(userOrg string, userId string, admin bool, vaultToken string) (policyName string, err error) {
@@ -140,15 +143,19 @@ func getPolicyName(userOrg string, userId string, admin bool) (policyName string
 // Construct the ACL policy rules based on the user and their admin status.
 func getPolicyString(userOrg string, userId string, admin bool) (policyString string) {
 	if admin {
-		adminPolicy := fmt.Sprintf(ADMIN_ORG_WIDE_POLICY, userOrg)
+		adminPolicy1 := fmt.Sprintf(ADMIN_ORG_WIDE_POLICY1, userOrg)
+		adminPolicy2 := fmt.Sprintf(ADMIN_ORG_WIDE_POLICY2, userOrg)
 		adminUserPrivatePolicy := fmt.Sprintf(ADMIN_USER_PRIVATE_POLICY, userOrg)
-		userPrivatePolicy := fmt.Sprintf(USER_PRIVATE_POLICY, userOrg, userId)
-		policyString = fmt.Sprintf("%s %s %s", adminPolicy, adminUserPrivatePolicy, userPrivatePolicy)
+		userPrivatePolicy1 := fmt.Sprintf(USER_PRIVATE_POLICY1, userOrg, userId)
+		userPrivatePolicy2 := fmt.Sprintf(USER_PRIVATE_POLICY2, userOrg, userId)
+		policyString = fmt.Sprintf("%s %s %s %s %s", adminPolicy1, adminPolicy2, adminUserPrivatePolicy, userPrivatePolicy1, userPrivatePolicy2)
 	} else {
 		nonAdminPolicy := fmt.Sprintf(NON_ADMIN_ORG_WIDE_POLICY, userOrg)
-		adminUserPrivateDeny := fmt.Sprintf(USER_PRIVATE_DENY_POLICY, userOrg)
-		userPrivatePolicy := fmt.Sprintf(USER_PRIVATE_POLICY, userOrg, userId)
-		policyString = fmt.Sprintf("%s %s %s", nonAdminPolicy, adminUserPrivateDeny, userPrivatePolicy)
+		adminUserPrivateDeny1 := fmt.Sprintf(USER_PRIVATE_DENY_POLICY1, userOrg)
+		adminUserPrivateDeny2 := fmt.Sprintf(USER_PRIVATE_DENY_POLICY2, userOrg)
+		userPrivatePolicy1 := fmt.Sprintf(USER_PRIVATE_POLICY1, userOrg, userId)
+		userPrivatePolicy2 := fmt.Sprintf(USER_PRIVATE_POLICY2, userOrg, userId)
+		policyString = fmt.Sprintf("%s %s %s %s %s", nonAdminPolicy, adminUserPrivateDeny1, adminUserPrivateDeny2, userPrivatePolicy1, userPrivatePolicy2)
 	}
 
 	return
